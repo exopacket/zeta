084dc2db8c543968954c4d06c7bc6566c7082b49
Ryan Fitzgerald <101024833+inteliense@users.noreply.github.com>
2022-08-25 13:21:54 -0400

    Add files via upload

---
+++ APIServerType.java
@@ -0,0 +1,23 @@
+package com.inteliense.trusty.server;
+
+public enum APIServerType {
+    REST,
+    ZERO_TRUST,
+    REST_SYNC,
+    REST_ASYNC,
+    REST_HYBRID,
+    ZERO_TRUST_SYNC,
+    ZERO_TRUST_ASYNC,
+    ZERO_TRUST_HYBRID
+}
+
+/*
+    REST_SYNC,
+    REST_ASYNC,
+    REST_WEBSOCKET,
+    REST_HYBRID,
+    ZERO_TRUST_SYNC,
+    ZERO_TRUST_ASYNC,
+    ZERO_TRUST_HYBRID,
+    ZERO_TRUST_WEBSOCKET
+ */
